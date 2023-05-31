3414c9efe0b803cf444edc4277245e5b3ad6d7f8
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-31 02:40:16 -0700

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+"""
+create a route /status on the object app_views that returns a JSON
+"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """ get status """
     return jsonify({'status': 'OK'})
+
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
+    """ get app stats """
     stats = {
             'amenities': storage.count('Amenity'),
             'cities': storage.count('City'),
